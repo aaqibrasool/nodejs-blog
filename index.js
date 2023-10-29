7c85ee30f8c80797b607e42a27f81568af0dd87d
aaqib <aaqib.rasool@funfoxprogram.com.au>
2023-10-29 16:23:51 +0500

    created the front and backend to save users in the db.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const path = require("path")
 const express = require("express")
+const mongoose = require("mongoose")
+
+const userRouter = require("./routes/user")
 
 const app = express()
 const PORT = 8000
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/blogify")
+  .then(() => console.log("mongo connected"))
+
 app.set("view engine", "ejs")
 app.set("views", path.resolve("./views"))
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => res.render("home"))
+app.use("/user", userRouter)
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
