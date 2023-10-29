cfaf34791b61bf0a7049ff053680083475cbcdb5
aaqib <aaqib.rasool@funfoxprogram.com.au>
2023-10-29 19:28:22 +0500

    showing links based on the user on the client

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const path = require("path")
 const express = require("express")
 const mongoose = require("mongoose")
+const cookieParser = require("cookie-parser")
 require("dotenv").config()
 
 const userRouter = require("./routes/user")
+const { checkForAuthentication } = require("./middlewares/authentication")
 
 const app = express()
 
@@ -16,8 +18,14 @@ mongoose
 app.set("view engine", "ejs")
 app.set("views", path.resolve("./views"))
 app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(checkForAuthentication("token"))
 
-app.get("/", (req, res) => res.render("home"))
+app.get("/", (req, res) =>
+  res.render("home", {
+    user: req.user,
+  })
+)
 app.use("/user", userRouter)
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
---
+++ middlewares/authentication.js
@@ -0,0 +1,21 @@
+const { validateToken } = require("../services/authentication")
+
+function checkForAuthentication(cookieName) {
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName]
+    if (!tokenCookieValue) {
+      return next()
+    }
+
+    try {
+      const userPayload = validateToken(tokenCookieValue)
+      req.user = userPayload
+    } catch (e) {}
+
+    return next()
+  }
+}
+
+module.exports = {
+  checkForAuthentication,
+}
