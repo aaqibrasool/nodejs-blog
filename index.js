76c98b5f93842047acf17c6cfd9e612364f4d26e
aaqib <aaqib.rasool@funfoxprogram.com.au>
2023-11-05 11:20:00 +0500

    added blog schema and its related endpoints and page.

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
+require("dotenv").config()
+
 const path = require("path")
 const express = require("express")
 const mongoose = require("mongoose")
 const cookieParser = require("cookie-parser")
-require("dotenv").config()
 
-const userRouter = require("./routes/user")
+const Blog = require("./models/blog")
+
+const userRoute = require("./routes/user")
+const blogRoute = require("./routes/blog")
+
 const { checkForAuthentication } = require("./middlewares/authentication")
 
 const app = express()
-
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/blogify")
-  .then(() => console.log("mongo connected"))
+  .connect(process.env.MONGO_URL)
+  .then((e) => console.log("MongoDB Connected"))
 
 app.set("view engine", "ejs")
 app.set("views", path.resolve("./views"))
+
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(checkForAuthentication("token"))
+app.use(express.static(path.resolve("./public")))
 
-app.get("/", (req, res) =>
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog.find({})
   res.render("home", {
     user: req.user,
+    blogs: allBlogs,
   })
-)
-app.use("/user", userRouter)
+})
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+app.use("/user", userRoute)
+app.use("/blog", blogRoute)
+
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`))
